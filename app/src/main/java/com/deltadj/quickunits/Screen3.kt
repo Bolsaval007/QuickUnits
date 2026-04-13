38b7f52fdb50c070f14e41be75f927b562739336
Bolsaval007 <ortizmateusj@gmail.com>
2026-04-13 00:19:26 -0500

    Commit 4: Pantallas base Longitud, Masa y Velocidad

---
+++ app/src/main/java/com/deltadj/quickunits/Screen3.kt
@@ -0,0 +1,19 @@
+package com.deltadj.quickunits
+
+import androidx.compose.foundation.layout.*
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.sp
+
+@Composable
+fun Screen3() {
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(text = "Velocidad", fontSize = 32.sp)
+    }
+}
